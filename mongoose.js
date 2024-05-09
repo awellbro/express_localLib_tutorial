da09bdd0b1bd51e6ddc0fd26771037a389e64a1c
Aaron <wellbrockaaron@gmail.com>
2024-05-08 22:05:04 -0700

    second commit?

---
+++ mongoose.js
@@ -0,0 +1,22 @@
+// const mongoose = require('mongoose');
+
+// mongoose.set("strictQuery", false);
+
+// const mongoDB = "mongodb+srv://awellbro:Awellbro081390@mdntut.id1mh6a.mongodb.net/";
+
+// main().catch((err)=> console.log(err));
+//     async function main(){
+//         await mongoose.connect(mongoDB);
+//     }
+
+
+
+
+
+//     const {MongoClient} = require('mongodb');
+
+// const uri = "mongodb+srv://awellbro:Awellbro081390@mdntut.id1mh6a.mongodb.net/";
+// const client = new MongoClient(uri);
+
+// const db = client.db('drinksDb');
+// const collection = db.collection('drinksDb');
